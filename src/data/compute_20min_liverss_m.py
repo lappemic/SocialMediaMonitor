b9acf5f18c6f80f94d3bfcae5ed993f8eb70b43a
lappemic <mi.lappert@gmail.com>
2022-05-09 17:08:47 +0200

    Changes of computing 20min

--- src/data/compute_20min_liverss_m.py
+++ src/data/compute_20min_liverss_m.py
@@ -15,17 +15,28 @@ print(f"Articles in feed : {len(feed.entries)}")
 
 # Get last check date from global last_check_articles_20min
 
-# Computing the newly aquired articles
+# Computing the newly aquired articles -> not used up to now (04.05.2022)
 articles_to_keep = []
 for entry in feed.entries:
     entry_date = pd.to_datetime(entry.published)
-    #print(f"Entry date : {entry_date}")
-    articles_to_keep.append(entry)
+    if last_check_articles_20min < entry_date:
+        articles_to_keep.append(entry)
 print(f"New articles in feed since last check : {len(articles_to_keep)}")
 
-# Compute a Pandas dataframe to write into 20min initialdataset
-liverss_df = pd.DataFrame.from_dict(articles_to_keep, orient='columns')
-liverss_df['published'] = pd.to_datetime(liverss_df['published'])
+# Compute temporary Pandas dataframe to further process beneath
+liverss_tmp_df = pd.DataFrame.from_dict(articles_to_keep, orient='columns')
+liverss_tmp_df['published'] = pd.to_datetime(liverss_tmp_df['published'])
+liverss_tmp_df.head()
+
+# Cleaning created dataframe of 20min liverss_tmp_df
+liverss_df = pd.DataFrame()
+liverss_df['title'] = liverss_tmp_df['title']
+liverss_df['summary'] = liverss_tmp_df['summary']
+liverss_df['link'] = liverss_tmp_df['link']
+liverss_df['id'] = liverss_tmp_df['id'].str[-12:]
+liverss_df['published'] = liverss_tmp_df['published']
+liverss_df['published_parsed'] = liverss_tmp_df['published_parsed']
+liverss_df.head()
 
 # Saving generated data to raw data folder
-liverss_df.to_csv(r'~/SocialMediaMonitor/data/raw/liverss_20min.csv', index = False)
+liverss_df.to_csv(r'~/Desktop/SocialMediaMonitor/data/raw/liverss_20Min.csv', index = False)
--- src/data/merge_20min_liverss_total_m.py
+++ src/data/merge_20min_liverss_total_m.py
@@ -14,18 +14,19 @@ and update the last_check_articles_20min variable
 '''
 
 # Reading the newly aquired articles in the temporary csv
-liverss_path = pathlib.Path.home() / 'SocialMediaMonitor' / 'data' / 'raw' / 'liverss_20min.csv'
+liverss_path = pathlib.Path.home() / 'Desktop' / 'SocialMediaMonitor' / 'data' / 'raw' / 'liverss_20min.csv'
 liverss_csv = open(liverss_path, 'r', newline = '')
 liverss_obj = csv.reader(liverss_csv)
 
 # Reading the log csv of articles and coms file and write the new articles to it
-liverss_and_coms_path = pathlib.Path.home() / 'SocialMediaMonitor' / 'data' / 'raw' / 'liverss_and_coms_20min.csv'
+liverss_and_coms_path = pathlib.Path.home() / 'Desktop' / 'SocialMediaMonitor' / 'data' / 'raw' / 'liverss_and_coms_20min.csv'
 liverss_and_coms_csv = open(liverss_and_coms_path, 'w', newline = '')
 liverss_and_coms_obj = csv.writer(liverss_and_coms_csv)
 liverss_and_coms_obj.writerows(liverss_obj)
 liverss_and_coms_csv.close()
 
 # Reading the newly enriched log csv and updating the last_check_articles_20min
-liverss_and_coms_path = pathlib.Path.home() / 'SocialMediaMonitor' / 'data' / 'raw' / 'liverss_and_coms_20min.csv'
+liverss_and_coms_path = pathlib.Path.home() / 'Desktop' / 'SocialMediaMonitor' / 'data' / 'raw' / 'liverss_and_coms_20min.csv'
 liverss_and_coms_df = pd.read_csv(liverss_and_coms_path)
 last_check_articles_20min = liverss_and_coms_df['published'].max()
+
